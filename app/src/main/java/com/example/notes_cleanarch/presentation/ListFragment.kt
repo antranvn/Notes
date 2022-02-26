dfc7ad189e134343a6bad88e423711c79b191595
antranvn <andevstudioth@gmail.com>
2022-02-26 13:53:35 +0700

    feat: setup view binding

--- app/src/main/java/com/example/notes_cleanarch/presentation/ListFragment.kt
+++ app/src/main/java/com/example/notes_cleanarch/presentation/ListFragment.kt
@@ -5,16 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.notes_cleanarch.R
+import com.example.notes_cleanarch.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+
+    private var _binding: FragmentListBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+    ): View {
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
