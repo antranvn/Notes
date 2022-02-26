e922a51a51ae3644146dda82600dd4e22277b5c8
antranvn <andevstudioth@gmail.com>
2022-02-26 13:13:15 +0700

    feat: add ktlint

--- app/src/main/java/com/example/notes_cleanarch/presentation/MainActivity.kt
+++ app/src/main/java/com/example/notes_cleanarch/presentation/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.notes_cleanarch.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.notes_cleanarch.R
 
 class MainActivity : AppCompatActivity() {
